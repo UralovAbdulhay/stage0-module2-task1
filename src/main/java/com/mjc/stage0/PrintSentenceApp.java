c50a5258127a2b487fb00c6b2a83035c29e66b2c
Abdulhay <abdulhayuralov8185603@gmail.com>
2022-07-23 10:36:07 +0500

    Task #1. Syntax

---
+++ src/main/java/com/mjc/stage0/PrintSentenceApp.java
@@ -0,0 +1,11 @@
+package com.mjc.stage0;
+
+public class PrintSentenceApp  {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("This \n is \n my \n first \n Java \n program");
+
+    }
+}
