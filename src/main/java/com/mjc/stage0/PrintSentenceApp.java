6bf6bb3d82e52fb32366db39a5397c374943789b
Abdulhay <abdulhayuralov8185603@gmail.com>
2022-07-23 10:41:02 +0500

    some bug fixed

--- src/main/java/com/mjc/stage0/PrintSentenceApp.java
+++ src/main/java/com/mjc/stage0/PrintSentenceApp.java
@@ -5,7 +5,7 @@ public class PrintSentenceApp  {
     public static void main(String[] args) {
 
 
-        System.out.println("This \n is \n my \n first \n Java \n program");
+        System.out.println("This \nis \nmy \nfirst \nJava \nprogram");
 
     }
 }
--- src/main/java/com/mjc/stage0/UpdatedApp.java
+++ src/main/java/com/mjc/stage0/UpdatedApp.java
@@ -2,6 +2,6 @@ package com.mjc.stage0;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("I'm \nupdated" + "\n\n" + "\ncan \nbe \ncompiled. \nAnd \ncan \nwrite \nto \nconsole");
+        System.out.println("I'm \nupdated" + "\n" + "\ncan \nbe \ncompiled. \nAnd \ncan \nwrite \nto \nconsole");
     }
 }
